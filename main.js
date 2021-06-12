2e792f50d3c9a4dc24fe238727d133c72ab58a3b
Diamond Nicholas <diamondnicholas154@gmail.com>
2021-06-12 05:27:53 +0100

    add some functions to js: getting usersData, saving to local storage, and appending to html

--- main.js
+++ main.js
@@ -9,6 +9,37 @@ function Book(title, author, pages, read) {
 }
 
 //add book to library
-function addBookToLibrary() {
-    
+const usersData = function addBookToLibrary(e) {
+    e.preventDefault(); //prevents the forms normal behaviour from submitting
+    let data = {
+        formTitle: document.querySelector('#title').value,
+        formAuthor: document.querySelector('#author').value,
+        formPages: document.querySelector('#pages').value,
+        formRead: document.querySelector('#read').value
+    }
+    myLibrary.push(data);
+    const formData = document.querySelector('#book-form');
+    formData.reset(); //resets the form value 
+
+    // console.log('added', {myLibrary});
+    // console.log(Object.values(myLibrary));
+
+
+    //append to html
+    const newBook = document.querySelector('#msg pre')
+    newBook.textContent = '\n' + JSON.stringify(Object.values(myLibrary), '\t', 2)
+
+
+    //saving to local storage
+    let storedData = localStorage.setItem('MyLibrabyList', JSON.stringify(myLibrary));
+    // console.log(storedData);
+
+
+    //
+    const show = document.querySelector('.store article');
+    show.textContent = storedData;
 }
+
+document.addEventListener('DOMContentLoaded', () =>{
+    document.querySelector('.submit').addEventListener('click', usersData);
+});
